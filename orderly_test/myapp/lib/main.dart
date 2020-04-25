daa34b6838c7bc829d2c8074d35d510f4f1fc2a5
Lawrence <lawrenceonen83@gmail.com>
2020-04-25 23:07:13 +0300

    json objects still failing to load

--- orderly_test/myapp/lib/main.dart
+++ orderly_test/myapp/lib/main.dart
@@ -1,85 +1,93 @@
 
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-//import 'package:http/http.dart' as http;
-void main() => runApp(MyApp());
+void main() => runApp(
+  MyApp()
+);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      home: Scaffold(
+        body: Homepage()
+      ),
     );
   }
 }
 
 class Homepage extends StatefulWidget {
   @override
- HomepageState createState() => new HomepageState();
+  HomepageState createState() => new HomepageState();
 }
 
-//Class to hold our data from the http response
-class CountryList {
-  final String name;
-  CountryList({this.name});
+class HomepageState extends State<Homepage> {
+  List<TimeDemoData> list = List();
+  bool isLoading = false;
 
-  factory CountryList.fromJson(Map<String, dynamic> json) {
-    return CountryList(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Timezone Demo"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+             onPressed: null
+             )
+        ],
+      ),
+      body: isLoading
+        ? Center(
+          child: _fetchData(),
+        )
+        : ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              contentPadding: EdgeInsets.all(10.2),
+              title: new Text(list[index]['name']),
+              trailing: Icon(
+                Icons.arrow_forward
+              ),
+            );
+          }
+          )
+    );
+  }
+//Method to fetch the service
+_fetchData() async {
+  setState(() {
+    isLoading = true;
+  });
+  const String url = 'http://worldtimeapi.org/api/timezone';
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    list = (json.decode(response.body) as List)
+    .toList();
+    setState(() {
+      isLoading = false;
+    });
+  } else {
+    throw Exception('Failed to load data');
+  }
+}//Fetch data
+
+}
+
+//Factory class to parse the Json data
+class TimeDemoData {
+  final String name;
+TimeDemoData._({this.name});
+factory TimeDemoData.fromJson(Map<String, dynamic> json) {
+    return new TimeDemoData._(
       name: json['name']
     );
   }
 }
-
-class HomepageState extends State<Homepage> {
-  //Fetch the data
-  Future<CountryList> countrylist;
-  @override
-  void initState() {
-    super.initState();
-    countrylist = fetchAlbum();
-  }
-//Make a network request
-Future<CountryList> fetchAlbum() async {
-  final response = await http.get('http://worldtimeapi.org/api/timezone"');
-  if (response.statusCode == 200) {
-    return CountryList.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Failed to load list');
-  }
-}
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Time Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Fetch Data Example'),
-        ),
-        body: Center(
-          child: FutureBuilder<CountryList>(
-  future: countrylist,
-  builder: (context, snapshot) {
-    if (snapshot.hasData) {
-      return Text(snapshot.data.name);
-    } else if (snapshot.hasError) {
-      return Text("${snapshot.error}");
-    }
-
-    // By default, show a loading spinner.
-    return CircularProgressIndicator();
-  },
-),
-        ),
-      )
-    );
-  }
-}
-
