50825881f21dbf9375c72609efa2d812a3f3cc62
Lawrence <lawrenceonen83@gmail.com>
2020-04-26 22:26:18 +0300

    refactored homepage to a different file

--- orderly_test/myapp/lib/homepage.dart
+++ orderly_test/myapp/lib/homepage.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Time Demo"),
-          centerTitle: true,
-        ),
-        body: Homepage(),
-      ),
-    );
-  }
-}
-
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -41,4 +24,5 @@ class Homepage extends StatelessWidget {
           ],
         );
       }
+  
 }
