dc39f8109bdf6ae331bf36c932b7b1abfdb920c9
Lawrence <lawrenceonen83@gmail.com>
2020-04-28 15:44:39 +0300

    refactored fetching of data to use future

--- orderly_test/myapp/lib/homepage.dart
+++ orderly_test/myapp/lib/homepage.dart
@@ -13,62 +13,48 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   DateTime localtime = new DateTime.now();
-  List<String> timezones = new List();
+  Future<Timezones> dataList;
 
+  @override
+  void initState() {
+    super.initState();
+    dataList = fectTimezone();
+  }
+  Future<Timezones> fectTimezone() async {
+  final response = await http.get('http://worldtimeapi.org/api/timezone');
+  if(response.statusCode == 200)
+  {
+    return Timezones.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('failed to load data');
+  }
+}
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-       appBar: AppBar(
-       ),
-       body: ListView.builder(
-         itemCount: timezones.length,
-         itemBuilder: (BuildContext context, int index) {
-           return ListTile(
-                  title: Text(fetch()),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                  onTap: () {
-                    //Do something
-                  } 
-                  );
-         }
-         ),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: FutureBuilder<Timezones>(
+            future: dataList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.title);
+              } else {
+                return Text("${snapshot.error}");
+              }
+            }
+            )
+        ),),
     );
   }
-
-  fetch() async {
-    final response = await http.get('http://worldtimeapi.org/api/timezone');
-    if (response.statusCode == 200) {
-      setState(() {
-        timezones.add(json.decode(response.body)); 
-      });
-    } else {
-      throw Exception('failed to load data');
-    }
-  }
- 
-  //Widget _zonalListView(BuildContext context) {
-    
-    // return ListView.builder(
-    //   itemCount: europeanCountries.length,
-    //   separatorBuilder: (BuildContext context, int index) => Divider(),
-    //   itemBuilder: (BuildContext context, int index) {
-    //     return ListTile(
-    //         title: Text(europeanCountries[index]),
-    //         trailing: Icon(Icons.arrow_forward_ios),
-    //         onTap: () {
-    //           _navigateToNextScreen(context, europeanCountries[index]);
-    //         } //=> print(europeanCountries[index]), // Pass this data to the next widget that handles the details
-    //         );
-    //   },
-    // );
-  //}
-
-  // _navigateToNextScreen(BuildContext context, String europeanCountries) {
-  //   Navigator.push(
-  //     context,
-  //     MaterialPageRoute(builder: (context) => Detailpage(countries: null,)),
-  //   );
-  // }
 }
+class Timezones {
+  final String title;
 
+  Timezones({this.title});
+
+  factory Timezones.fromJson(Map<String, dynamic> json) {
+    return Timezones();
+  }
+}
