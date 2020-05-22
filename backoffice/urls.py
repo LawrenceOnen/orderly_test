9d4164daa662849390f042845bea2b635c3174e3
Lawrence <lawrenceonen83@gmail.com>
2020-05-22 13:54:48 +0300

    auto wire the api urls in view

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from backoffice.webapp import views
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+#Wire up the urls using automatic URL routing
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    """path('admin/', admin.site.urls),
 ]
--- backoffice/webapp/views.py
+++ backoffice/webapp/views.py
@@ -5,8 +5,16 @@ from rest_framework import permissions
 from backoffice.webapp.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
+# ViewSets allows us to neatly organise the vew logic instead of spliting them into different views.
 class UserViewSet(viewsets.ModelViewSet):
     #API endpoint that allows users to be viewed or edited
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    #API endpoint that allows groups to be viewed or edited
+    queryset = User.objects.all()
+    serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
